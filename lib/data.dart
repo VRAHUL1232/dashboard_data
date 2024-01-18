23d413b02a9becfd3c07ce31d2aa23f37320ea76
vrahul <rahulgamings46@gmail.com>
2024-01-18 19:31:31 +0530

    table

--- lib/data.dart
+++ lib/data.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:csv/csv.dart';
 import 'dart:async' show Future;
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:excel/excel.dart';
 import 'package:flutter/services.dart';
 
 class DataScreen extends StatefulWidget {
@@ -53,43 +52,46 @@ Widget build(BuildContext context) {
     ),
     body: SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-        child: Table(
-          columnWidths: {
-            0: FixedColumnWidth(screenWidth*0.1),
-            1: FixedColumnWidth(screenWidth*0.1),
-            2: FixedColumnWidth(screenWidth*0.1),
-            3: FixedColumnWidth(screenWidth*0.1),
-            4: FixedColumnWidth(screenWidth*0.1),
-            5: FixedColumnWidth(screenWidth*0.1),
-            6: FixedColumnWidth(screenWidth*0.1),
-            7: FixedColumnWidth(screenWidth*0.1),
-            8: FixedColumnWidth(screenWidth*0.1),
-            9: FixedColumnWidth(screenWidth*0.1),
-            10: FixedColumnWidth(screenWidth*0.1),
-            11: FixedColumnWidth(screenWidth*0.1),
-            12: FixedColumnWidth(screenWidth*0.1),
-            13: FixedColumnWidth(screenWidth*0.1),
-            14: FixedColumnWidth(screenWidth*0.1),
-            15: FixedColumnWidth(screenWidth*0.1),
-            16: FixedColumnWidth(screenWidth*0.1),
-            17: FixedColumnWidth(screenWidth*0.1),
-            18: FixedColumnWidth(screenWidth*0.1)
-          },
-          border: TableBorder.all(width: 1.0),
-          children: csvData.map((item) {
-            return TableRow(
-                children: item.map((row) {
-              return Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    row.toString(),
-                    style: TextStyle(fontSize: 20.0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Table(
+            columnWidths: {
+              0: FixedColumnWidth(screenWidth*0.1),
+              1: FixedColumnWidth(screenWidth*0.1),
+              2: FixedColumnWidth(screenWidth*0.1),
+              3: FixedColumnWidth(screenWidth*0.1),
+              4: FixedColumnWidth(screenWidth*0.1),
+              5: FixedColumnWidth(screenWidth*0.1),
+              6: FixedColumnWidth(screenWidth*0.1),
+              7: FixedColumnWidth(screenWidth*0.1),
+              8: FixedColumnWidth(screenWidth*0.1),
+              9: FixedColumnWidth(screenWidth*0.1),
+              10: FixedColumnWidth(screenWidth*0.1),
+              11: FixedColumnWidth(screenWidth*0.1),
+              12: FixedColumnWidth(screenWidth*0.1),
+              13: FixedColumnWidth(screenWidth*0.1),
+              14: FixedColumnWidth(screenWidth*0.1),
+              15: FixedColumnWidth(screenWidth*0.1),
+              16: FixedColumnWidth(screenWidth*0.1),
+              17: FixedColumnWidth(screenWidth*0.1),
+              18: FixedColumnWidth(screenWidth*0.1)
+            },
+            border: TableBorder.all(width: 1.0),
+            children: csvData.map((item) {
+              return TableRow(
+                  children: item.map((row) {
+                return SizedBox(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      row.toString(),
+                      style: TextStyle(fontSize: 20.0),
+                    ),
                   ),
-                ),
-              );
-            }).toList());
-          }).toList(),
+                );
+              }).toList());
+            }).toList(),
+          ),
         ),
     ));
 }
